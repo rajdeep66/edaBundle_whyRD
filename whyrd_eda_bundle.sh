90107e1972af77537fb90d92defcac1406216012
Rajdeep Mazumder <31538079+rajdeep66@users.noreply.github.com>
2023-05-14 15:43:52 +0530

    Update whyrd_eda_bundle.sh

--- whyrd_eda_bundle.sh
+++ whyrd_eda_bundle.sh
@@ -68,30 +68,9 @@ make install
 cd ..
 
 
-#######################################################
-#STEP:5 Installing openPDK and sky130nm
-########################################################
-## fetch the repository with git:
-cd ~/whyRD_eda_bundle/
-git clone git://opencircuitdesign.com/open_pdks
-cd open_pdks
-## configure the build, a --prefix option can be given to install
-## in a different place, by default after installation a 
-## /usr/local/share/pdk directory is created if no --prefix is provided.
-## Below line for example requests installation in my home directory
-## (/home/schippes/share/pdk):
-## ./configure --enable-sky130-pdk --prefix=/home/schippes
-## Do the following steps one at a time and ensure no errors are
-##  reported after each step.
-./configure --enable-sky130-pdk 
-make
-make install 
-cd ..
 
 
-#integrate sky130 to magic 
-sudo ln -s /usr/local/share/pdk/sky130A/libs.tech/magic/* /usr/local/lib/magic/sys
-#magic -T sky130A
+
 
 
 
